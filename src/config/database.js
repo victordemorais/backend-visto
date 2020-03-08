08535874aff61651b53d4bfbc3b9bf8e50fa4ee8
Victor de Morais <victordemorais@hotmail.com.br>
2020-03-08 15:03:23 -0300

    Config docker

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: 'root',
-  database: 'visto_cars',
+  host: process.env.DB_SERVER,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   define: {
     timestamps: true,
     underscored: true,
